2e8118235fdd0f56f1b7977503fad08461203ed3
Mike Carter <elite.mikecarter@gmail.com>
2021-03-09 09:06:44 -0500

    edit jumbotron header

--- google-book-search/src/components/Jumbotron/index.js
+++ google-book-search/src/components/Jumbotron/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Jumbotron({ children }) {
+function Jumbotron() {
   return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
+    <div     
+      className="jumbotron text-center"
     >
-      {children}
+      <h1> Google Book Search</h1>
+      <h4>What shall we read today?</h4>
     </div>
   );
 }
