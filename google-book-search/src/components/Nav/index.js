6c14ed775bc8b8f35bad0f40bd987ec7cc1dbad9
Mike Carter <elite.mikecarter@gmail.com>
2021-03-09 22:39:45 -0500

    debugging nav

--- google-book-search/src/components/Nav/index.js
+++ google-book-search/src/components/Nav/index.js
@@ -1,13 +1,46 @@
+// import React from "react";
+
+// function Nav() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//       <a className="navbar-brand" href="/">
+//         React Reading List
+//       </a>
+//     </nav>
+//   );
+// }
+
+// export default Nav;
 import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
-  );
-}
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Google Books
+            </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/search"
+                    className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+                >
+                    Search
+            </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/saved"
+                    className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+                >
+                    Saved
+            </Link>
+            </li>
+        </ul>
+    );
+};
 
 export default Nav;
